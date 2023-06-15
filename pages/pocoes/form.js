76d7ecc483c08f8d054e748dc4dde5f39c48f872
João Leonardo <joaoleonardo9921@gmail.com>
2023-06-15 10:47:59 -0300

    adicionando crud de poções

--- pages/pocoes/form.js
+++ pages/pocoes/form.js
@@ -1,13 +1,80 @@
 import Header from '@/components/Header'
 import GlobalStyle from '@/styles/globalStyle'
-import React from 'react'
+import styleForm from '@/styles/styleForm'
+import geralValidator from '@/validators/geralValidator'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
+import { Button, Container, Form } from 'react-bootstrap'
+import { useForm } from 'react-hook-form'
+import { AiOutlinePlus } from 'react-icons/ai'
+import { RiFilePaperFill } from 'react-icons/ri'
+import { TbArrowLeftTail } from 'react-icons/tb'
 
-const formPocoes= () => {
+const formPocoes = () => {
+  const { push } = useRouter()
+  const { register, handleSubmit, formState : { errors }, setValue } = useForm();
+
+  function salvar(dados) {
+    console.log(dados);
+    const pocoes = JSON.parse(window.localStorage.getItem('pocoes')) || []
+    pocoes.unshift(dados)
+    window.localStorage.setItem('pocoes', JSON.stringify(pocoes))
+    push("/pocoes")
+  }
+  function handleChange(event){
+    setValue(event.target.name, (mask(event.target.value, event.target.getAttribute("mask"))))
+  }
   return (
     <> 
     <GlobalStyle/>
     <Header/>
-    <div>form poções</div>
+    <Container>
+    <div style={styleForm}>  
+    <Form>
+        <Form.Group className="py-2 px-3" controlId="tipo">
+          <Form.Label> Escolha o tipo da poção </Form.Label>
+          <Form.Select {...register('tipo')}>
+          <option value=""> Selecione o tipo da poção </option>
+          <option key={1} value={"Envenenemento"}> Envenenemento </option>   
+          <option key={2} value={"Paralizante"}> Paralizante </option>     
+          <option key={3} value={"Sonífera"}> Sonífera </option>           
+          </Form.Select>
+        </Form.Group>
+
+       
+        <Form.Group className="py-2 px-3" controlId="qtd">
+          <Form.Label>Quantidade</Form.Label>
+          <Form.Control type="number" placeholder="Digite a quantidade de poções que deseja " {...register('qtd', geralValidator.nome)}/>
+        </Form.Group>
+        
+        <Form.Group className="py-2 px-3" controlId="perigo">
+          <Form.Label> Nível de perigo</Form.Label>
+          <Form.Select {...register('perigo', geralValidator.nome)}>
+          <option value=""> Selecione o nível de perigo da poção</option>
+                <option key={"inofensivo"} value={"Inofensivo"}> Inofensivo </option>
+                <option key={"mediano"} value={"Mediano"}> Mediano </option>
+                <option key={"mortal"} value={"Mortal"}> Mortal </option>
+          </Form.Select>
+        </Form.Group>
+
+      
+
+        <div className='text-center me-2 py-3'>
+        <Button style={styleForm.buttonSave}type="button" className='me-2' onClick={handleSubmit(salvar)}>
+          <RiFilePaperFill/>
+          Salvar
+        </Button>
+        <Link href={'/pocoes'}>
+        <Button type="button" style={styleForm.buttonBack}>
+          <TbArrowLeftTail/> 
+          Voltar
+        </Button>
+        </Link>
+        </div>
+      </Form>
+      </div>
+       </Container>
     </>
   )
 }
--- pages/pocoes/index.js
+++ pages/pocoes/index.js
@@ -1,15 +1,77 @@
 import Header from '@/components/Header'
 import GlobalStyle from '@/styles/globalStyle'
-import React from 'react'
+import styleForm from '@/styles/styleForm'
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+import { Button, Container, Table } from 'react-bootstrap'
+import { AiOutlinePlus } from 'react-icons/ai'
+import { BsFillGearFill, BsTrashFill } from 'react-icons/bs'
+import { TbFeather, TbTrashFilled}  from 'react-icons/tb'
 
 const index = () => {
-  return (
-    <> 
-    <GlobalStyle/>
-    <Header/>
-    <div>poções</div>
-    </>
-  )
+  const [pocoes, setpocoes] = useState([])
+
+    useEffect(()=>{
+        setpocoes(getAll())
+    },[])
+
+    function getAll(){
+      return JSON.parse(window.localStorage.getItem('pocoes')) || []
+    }
+    function excluir(i){ 
+      if(confirm("Deseja realmente excluir o registro?")) { 
+        const itens = getAll()
+        itens.splice(i, 1) 
+        window.localStorage.setItem('pocoes', JSON.stringify(itens))
+        setpocoes(itens)
+    }
+    }
+    return (
+        <>
+        <GlobalStyle/>
+            <Header/>
+            <Container> 
+            <Link href="/pocoes/form" className='mb-2'> 
+            <Button style={styleForm.buttonPlus} className='mb-2' > 
+            <AiOutlinePlus />
+                Criar poção
+            </Button>
+            </Link>
+            <Table variant="secondary" className='text-light'>
+                <thead>
+                    <tr>                   
+                        <th> <BsFillGearFill/> </th>    
+                        <th>Tipo</th>
+                        <th>Quantidade</th>
+                        <th>Perigo</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {pocoes.map((p,index) =>(
+		            <tr key={index}>
+                        <td>
+                        <Link href={'/pocoes/' + index}>
+                            <TbFeather className='text-secondary' /> 
+                        </Link>
+                        <TbTrashFilled onClick={() => excluir(index)} className="text-danger me-2"/> 
+                        </td>
+                        <td>
+                            {p.tipo}
+                        </td>
+                        <td>
+                            {p.qtd}
+                        </td>
+                        <td>
+                            {p.perigo}
+                        </td> 
+                    </tr>
+                ))}
+                </tbody>
+            </Table>
+            </Container>
+        </>
+
+    )
 }
 
 export default index
