f31f4e46deef1a439f9e64174cb85effda39b264
João Leonardo <joaoleonardo9921@gmail.com>
2023-06-16 11:13:15 -0300

    adicionado crud de personagens

--- pages/personagens/form.js
+++ pages/personagens/form.js
@@ -1,13 +1,139 @@
 import Header from '@/components/Header'
 import GlobalStyle from '@/styles/globalStyle'
-import React from 'react'
+import styleForm from '@/styles/styleForm'
+import geralValidator from '@/validators/geralValidator'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
+import { Button, Container, Form } from 'react-bootstrap'
+import { useForm } from 'react-hook-form'
+import { AiOutlinePlus } from 'react-icons/ai'
+import { RiFilePaperFill } from 'react-icons/ri'
+import { TbArrowLeftTail } from 'react-icons/tb'
+import { mask } from 'remask'
 
 const formPersonagens = () => {
+  const { push } = useRouter()
+  const { register, handleSubmit, formState : { errors }, setValue } = useForm();
+  const [classes, setClasses] = useState([])
+  const [jogadores, setJogadores] = useState([])
+
+  useEffect(() => {
+    const classes = JSON.parse(window.localStorage.getItem('classes')) || [] ; 
+    const jogadores = JSON.parse(window.localStorage.getItem('jogadores')) || [] ; 
+    setClasses(classes); 
+    setJogadores(jogadores);
+  }, []);
+
+  function salvar(dados) {
+    console.log(dados);
+    const personagens = JSON.parse(window.localStorage.getItem('personagens')) || []
+    personagens.unshift(dados)
+    window.localStorage.setItem('personagens', JSON.stringify(personagens))
+    push("/personagens")
+  }
+  function handleChange(event){
+    setValue(event.target.name, (mask(event.target.value, event.target.getAttribute("mask"))))
+  }
   return (
     <> 
     <GlobalStyle/>
     <Header/>
-    <div>form personagens</div>
+    <Container>
+    <div style={styleForm}>  
+    <Form>
+    <Form.Group className="py-2 px-3" controlId="jogador">
+          <Form.Label> Jogador </Form.Label>
+          <Form.Select {...register('jogador')}>
+          <option value=""> Selecione quem vai jogar com esse personagm </option>
+                  {jogadores.map((j,i) => (
+                  <option key={i} value={j.nome}> {j.nome} </option>               
+          ))}      
+          </Form.Select>
+        </Form.Group>
+
+    <Form.Group className="py-2 px-3" controlId="classFav">
+          <Form.Label> Classe do personagem </Form.Label>
+          <Form.Select {...register('classe')}>
+          <option value=""> Selecione a classe </option>
+                  {classes.map((c,i) => (
+                  <option key={i} value={c.nome}> {c.nome} </option>               
+          ))}      
+          </Form.Select>
+        </Form.Group>
+    
+        <Form.Group className="py-2 px-3" controlId="nome">
+          <Form.Label>Nome</Form.Label>
+          <Form.Control type="text" isInvalid={errors.nome} placeholder="Digite o nome do personagem" {...register('nome', geralValidator.nome)}/>
+          { errors.nome && <p className='mt-1 text-danger'> {errors.nome.message} </p> } 
+        </Form.Group>
+
+        <Form.Group className="py-2 px-3" controlId="classFav">
+          <Form.Label> Raça do personagem </Form.Label>
+          <Form.Select {...register('raca', geralValidator.nome)}>
+          <option value=""> Selecione a raça do personagem </option>
+                <option key={1} value={"Humano"}> Humano </option>     
+                <option key={2} value={"Elfo"}>  Elfo </option>   
+                <option key={3} value={"Draconato"}> Draconato </option>   
+                <option key={4} value={"Goblin"}> Goblin </option>   
+                <option key={5} value={"Halfling"}> Halfling </option>   
+                <option key={6} value={"Tiefling"}> Tiefling </option>                 
+          </Form.Select>
+        </Form.Group>
+
+        <Form.Group className="py-2 px-3" controlId="classFav">
+          <Form.Label> Alinhamento Moral </Form.Label>
+          <Form.Select {...register('alinhamento', geralValidator.nome)}>
+          <option value=""> Selecione  alinhamento do seu personagem </option>
+                <option key={1} value={"Leal e Bom"}> Leal e Bom </option>     
+                <option key={2} value={"Neutro e Bom"}>  Neutro e Bom </option>   
+                <option key={3} value={"Caótico e Bom"}> Caótico e Bom </option>   
+                <option key={4} value={"Leal e Neutro"}> Leal e Neutro </option>   
+                <option key={5} value={"Neutro"}> Neutro </option>   
+                <option key={6} value={"Caótico e Neutro"}> Caótico e Neutro </option>       
+                <option key={7} value={"Leal e Mau"}> Leal e Mau </option>             
+                <option key={8} value={"Neutro e Mau"}> Neutro e Mau </option>   
+                <option key={9} value={"Caótico e Mau"}> Caótico e Mau </option>   
+          </Form.Select>
+        </Form.Group>
+
+        <Form.Group className="py-2 px-3" controlId="idade">
+          <Form.Label>Idade</Form.Label>
+          <Form.Control type="number"
+           max={3000}
+           min={0}
+           placeholder="Digite a idade do personagem" {...register('idade', geralValidator.nome)}/>
+        </Form.Group>
+
+        <Form.Group className="py-2 px-3" controlId="aparencia">
+          <Form.Label>Aparência Física</Form.Label>
+          <Form.Control type="text" placeholder="Descreva a aparência do seu personagem "  {...register('aparencia', geralValidator.nome)}/>
+        </Form.Group>
+
+        <Form.Group className="py-2 px-3" controlId="historia">
+          <Form.Label>História</Form.Label>
+          <Form.Control 
+          as="textarea"
+          type="text" 
+          placeholder="Conte um pouco da história do seu personagem " 
+          {...register('historia', geralValidator.nome)}/>
+        </Form.Group>
+
+        <div className='text-center me-2 py-3'>
+        <Button style={styleForm.buttonSave}type="button" className='me-2' onClick={handleSubmit(salvar)}>
+          <RiFilePaperFill/>
+          Salvar
+        </Button>
+        <Link href={'/personagens'}>
+        <Button type="button" style={styleForm.buttonBack}>
+          <TbArrowLeftTail/> 
+          Voltar
+        </Button>
+        </Link>
+        </div>
+      </Form>
+      </div>
+       </Container>
     </>
   )
 }
--- pages/personagens/index.js
+++ pages/personagens/index.js
@@ -1,15 +1,69 @@
 import Header from '@/components/Header'
 import GlobalStyle from '@/styles/globalStyle'
-import React from 'react'
+import styleCard from '@/styles/styleCard'
+import styleForm from '@/styles/styleForm'
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+import { Button, Card, Container, Table } from 'react-bootstrap'
+import { AiOutlinePlus } from 'react-icons/ai'
+import { BsFillGearFill, BsTrashFill } from 'react-icons/bs'
+import { TbFeather, TbTrashFilled}  from 'react-icons/tb'
 
 const index = () => {
-  return (
-    <> 
-    <GlobalStyle/>
-    <Header/>
-    <div>personagens</div>
-    </>
-  )
+  const [personagens, setpersonagens] = useState([])
+
+    useEffect(()=>{
+        setpersonagens(getAll())
+    },[])
+
+    function getAll(){
+      return JSON.parse(window.localStorage.getItem('personagens')) || []
+    }
+    function excluir(i){ 
+      if(confirm("Deseja realmente excluir o registro?")) { 
+        const itens = getAll()
+        itens.splice(i, 1) 
+        window.localStorage.setItem('personagens', JSON.stringify(itens))
+        setpersonagens(itens)
+    }
+    }
+    return (
+        <>
+        <GlobalStyle/>
+            <Header/>
+            <Container> 
+            <Link href="/personagens/form" className='mb-2'> 
+            <Button style={styleForm.buttonPlus} className='mb-2' > 
+            <AiOutlinePlus />
+                Criar novo personagem
+            </Button>
+            </Link>
+
+                {personagens.map((p,index) =>(
+		           <Card style={styleCard}>     
+                   <Card.Header as="h5">  {p.nome} </Card.Header>
+                   <Card.Body>
+                   <Card.Title> Jogador: {p.jogador}</Card.Title>
+                   <Card.Title> Classe: {p.classe}</Card.Title>
+                   <Card.Title> Raça: {p.raca} </Card.Title>
+                   <Card.Title> Alinhamento Moral: {p.alinhamento} </Card.Title>
+                   <Card.Title> Idade:  {p.idade} </Card.Title>
+                   <Card.Title> Aparência Física:  {p.aparencia} </Card.Title>
+                   <hr/>
+                   <Card.Title> História: </Card.Title>
+                   <Card.Text> <p size={25}> {p.historia} </p></Card.Text>
+                   <Link href={'/personagens/' + index}>
+                     <TbFeather size={25} className='text-light' /> 
+                     </Link>
+                     <TbTrashFilled size={25} onClick={() => excluir(index)} className="text-danger me-2"/> 
+                     </Card.Body>
+                    </Card> 		          
+                      ))}
+
+            </Container>
+        </>
+
+    )
 }
 
 export default index
