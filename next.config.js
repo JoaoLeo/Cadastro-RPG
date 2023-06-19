1ec0cd1031116e8bd934d1d4a9c3933780253f22
João Leonardo <joaoleonardo9921@gmail.com>
2023-06-19 12:53:10 -0300

    adicioando prefixo em rota de produção

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/Cadastro-RPG' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Cadastro-RPG/' : '',
 }
 
 module.exports = nextConfig
